297b109411c5f322d224db2db8c6132dff82feeb
qiuyangfeng <qq695702782@gmail.com>
2022-10-17 20:25:17 +0800

    2022101720

---
+++ fileStorage/views.py
@@ -0,0 +1,58 @@
+import os
+import uuid
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import FileUploadForm, FileUploadModelForm
+from .models import File
+
+def file_list(request):
+    """文件列表"""
+    files = File.objects.all().order_by('-id')
+    return render(request, 'fileStorage/file_list.html', {'files': files})
+
+
+def handle_uploaded_file(file):
+    ext = file.name.split(',')[-1]
+    file_name = "{}.{}".format(uuid.uuid4().hex[:10], ext)
+    file_path = os.path.join('media', 'files', file_name)
+    absolute_file_path = os.path.join('media', 'files', file_name)
+    directory = os.path.dirname(absolute_file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(absolute_file_path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+    return file_path
+
+
+def file_upload(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload_method = form.cleaned_data.get("upload_method")
+            raw_file = form.cleaned_data.get('file')
+            new_file = File()
+            new_file.file = handle_uploaded_file(raw_file)
+            new_file.upload_method = upload_method
+            new_file.save()
+            return redirect("/file/")
+    else:
+        form = FileUploadForm()
+    return render(request, 'upload_form.html', {'form': form, 'heading': 'Upload files with Regular Form'})
+
+
+def model_form_upload(request):
+    if request.method == "POST":
+        form = FileUploadModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()  # 一句话足以
+            return redirect("/account/file/")
+    else:
+        form = FileUploadModelForm()
+
+    return render(request, 'upload_form.html',
+                  {'form': form, 'heading': 'Upload files with ModelForm'}
+                  )
