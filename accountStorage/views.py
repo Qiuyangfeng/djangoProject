a7dd6dd454e7b34fcef5ec4decee8e015e19a39a
qiuyangfeng <qq695702782@gmail.com>
2022-10-18 09:40:06 +0800

    2022101809

--- accountStorage/views.py
+++ accountStorage/views.py
@@ -1,5 +1,3 @@
-import os
-import uuid
 import pandas as pd
 
 from django.core.paginator import Paginator
@@ -7,11 +5,11 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
-from .forms import FileUploadForm, FileUploadModelForm, UserModelForm
+from .forms import UserModelForm
 from .models import AccountPassword
-from .models import File
 
-def account_list(request):
+
+def account_list (request):
     """用户列表"""
     # for i in range(20):
     #     account = {
@@ -41,11 +39,11 @@ def account_list(request):
         "page_obj": page_obj,
         "form": form
     }
-    return render(request, 'account.html', context)
+    return render(request, 'accountStorage/account.html', context)
 
 
 @csrf_exempt
-def account_add(request):
+def account_add (request):
     """添加用户 (ajax请求)"""
     form = UserModelForm(data=request.POST)
     if form.is_valid():
@@ -57,7 +55,7 @@ def account_add(request):
     return JsonResponse({'status': False, 'error': form.errors})
 
 
-def account_detail(request):
+def account_detail (request):
     """获取账号详情"""
     uid = request.GET.get("uid")
     row_dict = AccountPassword.objects.filter(id=uid).values("name", "username", "password", "note").first()
@@ -68,7 +66,7 @@ def account_detail(request):
 
 
 @csrf_exempt
-def account_edit(request):
+def account_edit (request):
     """账号编辑"""
     uid = request.GET.get("uid")
     row_object = AccountPassword.objects.filter(id=uid).first()
@@ -81,7 +79,7 @@ def account_edit(request):
     return JsonResponse({"status": False, 'error': form.errors})
 
 
-def account_delete(request):
+def account_delete (request):
     """账号删除"""
     uid = request.GET.get('uid')
     exists = AccountPassword.objects.filter(id=uid).exists()
@@ -91,81 +89,25 @@ def account_delete(request):
     return JsonResponse({"status": True, 'msg': "删除成功"})
 
 
-def file_list(request):
-    """文件列表"""
-    files = File.objects.all().order_by('-id')
-    return render(request, 'file_list.html', {'files': files})
-
-
-def handle_uploaded_file(file):
-    ext = file.name.split(',')[-1]
-    file_name = "{}.{}".format(uuid.uuid4().hex[:10], ext)
-    file_path = os.path.join('media', 'files', file_name)
-    absolute_file_path = os.path.join('media', 'files', file_name)
-    directory = os.path.dirname(absolute_file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    with open(absolute_file_path, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
-
-    return file_path
-
-
-def file_upload(request):
-    if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            upload_method = form.cleaned_data.get("upload_method")
-            raw_file = form.cleaned_data.get('file')
-            new_file = File()
-            new_file.file = handle_uploaded_file(raw_file)
-            new_file.upload_method = upload_method
-            new_file.save()
-            return redirect("/account/file/")
-    else:
-        form = FileUploadForm()
-    return render(request, 'upload_form.html', {'form': form, 'heading': 'Upload files with Regular Form'})
-
-
-def model_form_upload(request):
-    if request.method == "POST":
-        form = FileUploadModelForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()  # 一句话足以
-            return redirect("/account/file/")
-    else:
-        form = FileUploadModelForm()
-
-    return render(request, 'upload_form.html',
-                  {'form': form, 'heading': 'Upload files with ModelForm'}
-                  )
-
-
-def upload_excel(request):
+def upload_excel (request):
     """上传excel，读取数据写入数据库"""
     if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            raw_file = form.cleaned_data.get('file')
-            print(raw_file)
-            df = pd.read_excel(raw_file)
-            print(df)
-            df.fillna("", inplace=True)
-            print(df.index.name)
-            for i in df.index.values:
-                df_dict = df.loc[i, ["name", "username", "password", "note"]].to_dict()
-                print(df_dict)
-                AccountPassword.objects.create(**df_dict)
-            return redirect("/account/")
-    else:
-        form = FileUploadForm()
-    return render(request, 'upload_excel.html', {'form': form, 'heading': 'Upload files with Regular Form'})
+        raw_file = request.FILES.get('file')
+        print(raw_file)
+        df = pd.read_excel(raw_file)
+        print(df)
+        df.fillna("", inplace=True)
+        print(df.index.name)
+        for i in df.index.values:
+            df_dict = df.loc[i, ["name", "username", "password", "note"]].to_dict()
+            print(df_dict)
+            AccountPassword.objects.create(**df_dict)
+        return redirect("/account/")
+    return render(request, 'accountStorage/upload_excel.html')
 
 
 @csrf_exempt
-def upload_ajax_excel(request):
+def upload_ajax_excel (request):
     """ajax上传excel，读取数据写入数据库"""
     if request.method == 'POST':
         file_object = request.FILES.get('files')
@@ -176,4 +118,4 @@ def upload_ajax_excel(request):
             print(df_dict)
             AccountPassword.objects.create(**df_dict)
         return redirect("/account/")
-    return render(request, 'ajax_upload.html')
+    return render(request, 'accountStorage/ajax_upload.html')
